751a38a3bdab8cbbca01f168eb7dd9e00230344a
Dave Denlow <Davedenlow@gmail.com>
2016-12-01 21:37:16 -0800

    add namespace, public class, html pages

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -1,1 +1,20 @@
+using Nancy;
+using System.Collections.Generic;
+using Anagram.Objects;
 
+namespace Instances
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get{"/"} = _ => {
+        return View["index.cshtml"];
+      };
+      Post["/form"] = _ => {
+        InstanceGenerator newInstanceGenerator = new InstanceGenerator(Request.Form["inputWords"]);
+        return View ["result.cshtml", newInstanceGenerator];
+      };
+    }
+  }
+}
--- Objects/Instances.cs
+++ Objects/Instances.cs
@@ -4,6 +4,26 @@ using System.Linq;
 
 namespace Instances.Objects
 {
-  public class InstanceGenerator  
+  public class InstanceGenerator
+  {
+
+    private string _wordOriginal;
+    private List<string> _letters = new List<string>{};
+
+    public InstanceGenerator(string word)
+    {
+      _wordOriginal = word;
+      Char[] charArray = word.ToUpper().ToCharArray();
+      foreach (char letter in charArray)
+      {
+        _letters.Add(letter.ToString());
+      }
+      if _letters.Match();
+    }
+
+    public List<string> LetterList()
+    {
+      return _letters;
+    }
   }
 }
